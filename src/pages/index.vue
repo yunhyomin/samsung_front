1cd2abe9292e717b11ae8a3b77c5fce684268152
poh <qudansdl@gmail.com>
2022-05-18 08:44:39 +0900

    메인 디자인 적용

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,79 +1,23 @@
 <script setup lang="ts">
-import { useUserStore } from '~/stores/user'
-
-const user = useUserStore()
-const name = $ref(user.savedName)
-
-const router = useRouter()
-const go = () => {
-  if (name)
-    router.push(`/hi/${encodeURIComponent(name)}`)
-}
-
 const { t } = useI18n()
-
-const title = computed<string>(() => import.meta.env.VITE_TITLE);
 </script>
 
 <template>
-
-  <div>
-    <div text-4xl>
-      <div i-carbon-campsite inline-block />
+  <div class="intoContainer">
+    <div class="visual">
+      <h2 class="visualTit" v-html="t('intro.welcome')">
+      </h2>
+      <p class="visualSubTit">{{ t('intro.visual-sub-tit') }}</p>
+      <article class="introBtn">
+        <button  class="whiteBtn">{{ t('intro.login') }}</button>
+        <button class="darkBtn">{{ t('intro.regist') }}</button>
+      </article>
     </div>
-    <p>
-      <a rel="noreferrer" href="https://github.com/antfu/vitesse" target="_blank">
-        {{ title }}
-      </a>
-    </p>
-    <p>
-      <em text-sm opacity-75>{{ t('intro.desc') }}</em>
-    </p>
 
-    <div py-4 />
-
-    <input
-      id="input"
-      v-model="name"
-      :placeholder="t('intro.whats-your-name')"
-      :aria-label="t('intro.whats-your-name')"
-      type="text"
-      autocomplete="false"
-      p="x4 y2"
-      w="250px"
-      text="center"
-      bg="transparent"
-      border="~ rounded gray-200 dark:gray-700"
-      outline="none active:none"
-      @keydown.enter="go"
-    >
-    <label class="hidden" for="input">{{ t('intro.whats-your-name') }}</label>
-
-    <div>
-      <button
-        btn m-3 text-sm
-        :disabled="!name"
-        @click="go"
-      >
-        {{ t('button.go') }}
-      </button>
-    </div>
   </div>
 </template>
 
 <route lang="yaml">
 meta:
-  layout: home
+  layout: index
 </route>
-
-<style lang="scss">
-.header {
-  width: 100%;
-  min-height: 56px;
-  background: rgb(0, 140, 255);
-  color: white;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-</style>
